b0f7661ddcd3db1153e86b16cb975a6c0bf72af8
amazingguni <amazingguni@gmail.com>
2017-07-03 23:58:17 +0900

    Init repository

---
+++ LedDriver/include/LedDriver.h
@@ -0,0 +1,5 @@
+void LedDriver_Create(uint16_t* address);
+void LedDriver_TurnOn(int ledNumber);
+void LedDriver_TurnAllOn(void);
+void LedDriver_TurnOff(int ledNumber);
+void LedDriver_Destroy(void);
---
+++ LedDriver/src/LedDriver.c
@@ -0,0 +1,48 @@
+#include <stdint.h>
+#include "LedDriver.h"
+
+
+
+enum {ALL_LEDS_ON = ~0, ALL_LEDS_OFF = ~ALL_LEDS_ON};
+
+static uint16_t * ledsAddress;
+static uint16_t ledsImage;
+
+static void updateHardware(void)
+{
+	*ledsAddress = ledsImage;
+}
+
+void LedDriver_Create(uint16_t * address)
+{
+	ledsAddress = address;
+	ledsImage = ALL_LEDS_OFF;
+	updateHardware();
+}
+
+static uint16_t convertLedNumberToBit(int ledNumber)
+{
+	return 1 << (ledNumber - 1);
+}
+
+void LedDriver_TurnOn(int ledNumber)
+{
+	ledsImage |= convertLedNumberToBit(ledNumber);
+	updateHardware();
+}
+
+void LedDriver_TurnAllOn(void)
+{
+	ledsImage = ALL_LEDS_ON;
+	updateHardware();
+}
+
+void LedDriver_TurnOff(int ledNumber)
+{
+	ledsImage &= ~(convertLedNumberToBit(ledNumber));
+	updateHardware();
+}
+
+void LedDriver_Destroy(void)
+{
+}
