93d8b5f887c547c86c136953cc8c29828673bf13
amazingguni <amazingguni@gmail.com>
2017-07-10 23:16:08 +0900

    Finish Chapter 4

--- LedDriver/src/LedDriver.c
+++ LedDriver/src/LedDriver.c
@@ -1,12 +1,16 @@
 #include <stdint.h>
 #include "LedDriver.h"
-
-
+#include "RuntimeError.h"
 
 enum {ALL_LEDS_ON = ~0, ALL_LEDS_OFF = ~ALL_LEDS_ON};
+enum {FIRST_LED = 1, LAST_LED = 16};
 
 static uint16_t * ledsAddress;
 static uint16_t ledsImage;
+
+static int isLedOutOfBounds(int ledNumber){
+	return (ledNumber < FIRST_LED) || (ledNumber > LAST_LED);
+}
 
 static void updateHardware(void)
 {
@@ -25,9 +29,17 @@ static uint16_t convertLedNumberToBit(int ledNumber)
 	return 1 << (ledNumber - 1);
 }
 
+static void setLedImageBit(int ledNumber){
+	ledsImage |= convertLedNumberToBit(ledNumber);
+}
+
 void LedDriver_TurnOn(int ledNumber)
 {
-	ledsImage |= convertLedNumberToBit(ledNumber);
+	if(isLedOutOfBounds(ledNumber)){
+		RUNTIME_ERROR("LED Driver: out-of-bounds LED", ledNumber);
+		return;
+	}
+	setLedImageBit(ledNumber);
 	updateHardware();
 }
 
@@ -37,10 +49,34 @@ void LedDriver_TurnAllOn(void)
 	updateHardware();
 }
 
-void LedDriver_TurnOff(int ledNumber)
+static void clearLedImageBit(int ledNumber)
 {
 	ledsImage &= ~(convertLedNumberToBit(ledNumber));
+}
+
+void LedDriver_TurnOff(int ledNumber)
+{
+	if(isLedOutOfBounds(ledNumber)){
+		RUNTIME_ERROR("LED Driver: out-of-bounds LED", ledNumber);
+		return;
+	}
+	clearLedImageBit(ledNumber);
 	updateHardware();
+}
+
+void LedDriver_TurnAllOff(void){
+	ledsImage = ALL_LEDS_OFF;
+	updateHardware();
+}
+
+int LedDriver_IsOn(int ledNumber){
+	if(isLedOutOfBounds(ledNumber))
+		return 0;
+	return ledsImage & (convertLedNumberToBit(ledNumber));
+}
+
+int LedDriver_IsOff(int ledNumber){
+	return !LedDriver_IsOn(ledNumber);
 }
 
 void LedDriver_Destroy(void)
--- LedDriver/test/LedDriverTest.c
+++ LedDriver/test/LedDriverTest.c
@@ -1,6 +1,7 @@
 #include "unity_fixture.h"
 #include "unity.h"
 #include "LedDriver.h"
+#include "RuntimeErrorStub.h"
 
 TEST_GROUP(LedDriver);
 
@@ -59,4 +60,79 @@ TEST(LedDriver, LedMemoryIsNotReadable)
 	virtualLeds = 0xffff;
 	LedDriver_TurnOn(8);
 	TEST_ASSERT_EQUAL_HEX16(0x80, virtualLeds);
+}
+
+TEST(LedDriver, UpperAndLowerBounds)
+{
+	LedDriver_TurnOn(1);
+	LedDriver_TurnOn(16);
+	TEST_ASSERT_EQUAL_HEX16(0x8001, virtualLeds);
+}
+
+TEST(LedDriver, OutOfBoundsTurnOnDoesNoHarm)
+{
+	LedDriver_TurnOn(-1);
+	LedDriver_TurnOn(0);
+	LedDriver_TurnOn(17);
+	LedDriver_TurnOn(3141);
+	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
+}
+
+TEST(LedDriver, OutOfBoundsTurnOffDoesNoHarm)
+{
+	LedDriver_TurnAllOn();
+	LedDriver_TurnOff(-1);
+	LedDriver_TurnOff(0);
+	LedDriver_TurnOff(17);
+	LedDriver_TurnOff(3141);
+	TEST_ASSERT_EQUAL_HEX16(0xffff, virtualLeds);
+}
+
+TEST(LedDriver, OutOfBoundsProducesRuntimeError)
+{
+	LedDriver_TurnOn(-1);
+	TEST_ASSERT_EQUAL_STRING("LED Driver: out-of-bounds LED",
+		RuntimeErrorStub_GetLastError());
+	TEST_ASSERT_EQUAL(-1, RuntimeErrorStub_GetLastParameter());
+}
+
+IGNORE_TEST(LedDriver, OutOfBoundsToDo){
+	/* TODO: 런타임 중에 어떻게 해야 하나? */
+}
+
+TEST(LedDriver, IsOn)
+{
+	TEST_ASSERT_FALSE(LedDriver_IsOn(11));
+	LedDriver_TurnOn(11);
+	TEST_ASSERT_TRUE(LedDriver_IsOn(11));
+}
+
+TEST(LedDriver, OutOfBoundsLedsAreAlwaysOff)
+{
+	TEST_ASSERT_TRUE(LedDriver_IsOff(0));
+	TEST_ASSERT_TRUE(LedDriver_IsOff(17));
+	TEST_ASSERT_FALSE(LedDriver_IsOn(0));
+	TEST_ASSERT_FALSE(LedDriver_IsOn(17));
+}
+
+TEST(LedDriver, IsOff)
+{
+	TEST_ASSERT_TRUE(LedDriver_IsOff(12));
+	LedDriver_TurnOn(12);
+	TEST_ASSERT_FALSE(LedDriver_IsOff(12));
+}
+
+TEST(LedDriver, TurnOffMultipleLeds)
+{
+	LedDriver_TurnAllOn();
+	LedDriver_TurnOff(9);
+	LedDriver_TurnOff(8);
+	TEST_ASSERT_EQUAL_HEX16((~0x180) & 0xffff, virtualLeds);
+}
+
+TEST(LedDriver, AllOff)
+{
+	LedDriver_TurnAllOn();
+	LedDriver_TurnAllOff();
+	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
 }
